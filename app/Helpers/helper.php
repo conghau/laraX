d0b0ba2b3fc9166f44beedcf2c29ec7262adebcd
HauTruong <hau.tc@mobivi.vn>
2016-10-19 17:07:42 +0700

    Udate

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -7,43 +7,79 @@
  */
 
 if (!function_exists('laraX_get_value')) {
-  function laraX_get_value($data, $key, $value_default = '') {
-    $sub_fields = explode('.', $key);
-    foreach ($sub_fields as $k) {
-      if (is_array($data)) {
-        $data = isset($data[$k]) ? ($data[$k]) : $value_default;
-      }
-      elseif (is_object($data)) {
-        $data = isset($data->{$k}) ? ($data->{$k}) : $value_default;
-      }
-      else {
-        $data = $value_default;
-        break;
-      }
+    function laraX_get_value($data, $key, $value_default = '') {
+        $sub_fields = explode('.', $key);
+        foreach ($sub_fields as $k) {
+            if (is_array($data)) {
+                $data = isset($data[$k]) ? ($data[$k]) : $value_default;
+            }
+            elseif (is_object($data)) {
+                $data = isset($data->{$k}) ? ($data->{$k}) : $value_default;
+            }
+            else {
+                $data = $value_default;
+                break;
+            }
+        }
+        return $data;
     }
-    return $data;
-  }
 }
 
 if (!function_exists('laraX_build_button_confirmation')) {
-  function laraX_build_button_confirmation($uri_link = '/', $title = '', $more_class = 'blue', $i_class_icon = 'fa fa-check') {
-    $output = '<button type="button" data-toggle="confirmation" data-ajax="' . asset($uri_link) . '" class="btn btn-outline btn-sm ajax-link ' . $more_class . '" title="' . $title . '"><i class="' . $i_class_icon . '"></i></button>';
-    return $output;
-  }
+    function laraX_build_button_confirmation($uri_link = '/', $title = '', $more_class = 'blue', $i_class_icon = 'fa fa-check') {
+        $output = '<button type="button" data-toggle="confirmation" data-ajax="' . asset($uri_link) . '" class="btn btn-outline btn-sm ajax-link ' . $more_class . '" title="' . $title . '"><i class="' . $i_class_icon . '"></i></button>';
+        return $output;
+    }
 }
 
 if (!function_exists('laraX_build_button')) {
-  function laraX_build_button($uri_link = '/', $title = '', $more_class = 'blue', $i_class_icon = 'icon-pencil') {
-    $output = '<a href="' . asset($uri_link) . '" class="btn btn-outline btn-sm ' . $more_class . '"><i class="' . $i_class_icon . '" title="' . $title . '"></i></a>';
-    return $output;
-  }
+    function laraX_build_button($uri_link = '/', $title = '', $more_class = 'blue', $i_class_icon = 'icon-pencil') {
+        $output = '<a href="' . asset($uri_link) . '" class="btn btn-outline btn-sm ' . $more_class . '"><i class="' . $i_class_icon . '" title="' . $title . '"></i></a>';
+        return $output;
+    }
 }
 
 if (!function_exists('laraX_build_url')) {
-  function laraX_build_url($path = '/', $is_admin_link = TRUE) {
-    if ($is_admin_link) {
-      $path = Illuminate\Support\Facades\Config::get('app.admin_path') . '/' . $path;
+    function laraX_build_url($path = '/', $is_admin_link = TRUE) {
+        if ($is_admin_link) {
+            $path = Illuminate\Support\Facades\Config::get('app.admin_path') . '/' . $path;
+        }
+        return asset($path);
     }
-    return asset($path);
-  }
+}
+
+if (!function_exists('laraX_isEmpty')) {
+    /**
+     * Check array or object is empty
+     *
+     * @param $o
+     * @return bool
+     */
+    function laraX_isNullOrEmpty($o) {
+        if (is_null($o)) {
+            return TRUE;
+        }
+        if (empty($o)) {
+            return TRUE;
+        }
+        if (is_numeric($o)) {
+            return FALSE;
+        }
+        if (is_string($o)) {
+            return !strlen(trim($o));
+        }
+        if (is_object($o)) {
+            return laraX_isNullOrEmpty((array) $o);
+        }
+
+        if (is_array($o)) {
+            foreach ($o as $element) {
+                if (!laraX_isNullOrEmpty($element)) {
+                    return FALSE;
+                }
+            }
+        }
+
+        return TRUE;
+    }
 }
--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -8,15 +8,110 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Repositories\MenuRepositoryInterface;
+use Illuminate\Http\Request;
 
-use App\Repositories\MenuRepository;
-
+/**
+ * Class MenuController
+ * @package App\Http\Controllers\Admin
+ */
 class MenuController extends BaseAdminController {
 
   protected $menuRepo;
 
-  public function __construct(MenuRepository $menuRepository) {
+  public function __construct(MenuRepositoryInterface $menuRepository) {
     parent::__construct();
     $this->menuRepo = $menuRepository;
+    $this->bodyClass = 'menu-controller';
+    $this->routeLink = 'menus';
+    $this->setPageTitle('Menu Management');
   }
+
+  public function getIndex(Request $request) {
+    return view('admin.menus.index');
+  }
+  
+  public function postIndex() {
+    
+  }
+  
+  public function getEdit(Request $request, $id) {
+    $this->data['object'] = new \stdClass();
+    $oldInputs = old();
+    if ($oldInputs && $id == 0) {
+      $oldObject = new \stdClass();
+      foreach ($oldInputs as $key => $row) {
+        $oldObject->$key = $row;
+      }
+      $this->data['object'] = $oldObject;
+    }
+
+//    $currentEditLanguage = Models\Language::getBy([
+//      'id' => $language,
+//      'status' => 1,
+//    ]);
+//    if (!$currentEditLanguage) {
+//      $this->_setFlashMessage('This language it not supported', 'error');
+//      $this->_showFlashMessages();
+//      return redirect()->back();
+//    }
+    $this->data['currentEditLanguage'] = 1;
+
+    $this->data['rawUrlChangeLanguage'] = asset($this->adminPath . '/' . $this->routeLink . '/edit/' . $id) . '/';
+
+//    $menu = $object->find($id);
+//    if (!$menu) {
+//      $menu = new Menu();
+//      $menuContent = null;
+//    } else {
+//      $menuContent = $objectContent->findByFieldsOrCreate([
+//        'menu_id' => $menu->id,
+//        'language_id' => $language,
+//      ]);
+//    }
+
+//    $this->setPageTitle('Menu', $menu->title);
+//
+//    $this->data['object'] = $menu;
+//
+//    $this->data['pages'] = Models\Page::getBy([
+//      'status' => 1,
+//    ], [
+//      'global_title' => 'ASC',
+//    ], true);
+
+    //$this->data['categories'] = $this->_getCategoriesSelectSrc($category, 'category', 0);
+    //$this->data['productCategories'] = $this->_getCategoriesSelectSrc($productCategory, 'product-category', 0);
+
+    //$this->data['nestableMenuSrc'] = $this->getNestableMenuSrc($menuContent, 0);
+
+    return view('admin.menus.edit', $this->data);
+  }
+
+  private function getNestableMenuSrc($menu, $parent_id)
+  {
+    if (!$menu) {
+      return '';
+    }
+
+    $menu_nodes = $this->getMenuNodes($menu->id, $parent_id);
+    $html_src = view('admin._partials.menu._nestable-menu-src', [
+      'menuNodes' => $menu_nodes,
+    ])->render();
+    return $html_src;
+  }
+
+  private function getMenuNodes($menu_content_id, $parent_id)
+  {
+    if (!$menu_content_id) {
+      return [];
+    }
+
+    $menu_nodes = MenuNode::getBy([
+      'menu_content_id' => $menu_content_id,
+      'parent_id' => $parent_id,
+    ], ['position' => 'ASC'], true);
+    return $menu_nodes;
+  }
+  
 }
